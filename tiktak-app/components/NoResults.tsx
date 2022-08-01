6971e2223c33e714cf418994729c014c67b845c0
kzo <bartosz.krzeminski24@gmail.com>
2022-08-02 00:36:27 +0200

    Configuration + start Fetching
    Basic review of configuration and start fetching video data.

---
+++ tiktak-app/components/NoResults.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NoResults = () => {
+  return <div>No Results</div>
+}
+
+export default NoResults
---
+++ tiktak-app/pages/index.tsx
@@ -0,0 +1,37 @@
+import type { NextPage } from 'next'
+import axios from 'axios'
+
+import VideoCard from '../components/VideoCard'
+// import { BASE_URL } from '../utils'
+import { Video } from '../types'
+import NoResults from '../components/NoResults'
+
+interface IProps {
+  videos: Video[]
+}
+
+const Home = ({ videos }: IProps) => {
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos?.map((video: Video) => (
+          <VideoCard post={video} isShowingOnHome key={video._id} />
+        ))
+      ) : (
+        <NoResults text={`No Videos`} />
+      )}
+    </div>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post/`)
+
+  return {
+    props: {
+      videos: data,
+    },
+  }
+}
+
+export default Home
